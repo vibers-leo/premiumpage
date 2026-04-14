accbccd9d642919107e0151ce35406c93715489b
vibers-leo <juuuno1116@gmail.com>
2026-04-15 08:15:44 +0900

    fix: ESLint no-explicit-any 등 warn으로 완화

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "react-hooks/exhaustive-deps": "warn",
+      "react/no-unescaped-entities": "warn",
+      "react-hooks/rules-of-hooks": "warn",
+    },
+  },
 ]);
 
 export default eslintConfig;
